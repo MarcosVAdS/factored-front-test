370d9992399374ff09951b310102d75066eb18f6
MarcosVAdS <victor.kernel14@gmail.com>
2025-03-23 00:31:53 -0300

    feature: create and implement the filter component

---
+++ src/app/global/components/filter/filter.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+ 
+@Component({
+  selector: 'app-filter',
+  imports: [FormsModule],
+  templateUrl: './filter.component.html',
+  styleUrl: './filter.component.css'
+})
+export class FilterComponent {
+
+  search: string = '';
+
+  @Output() searchOutput = new EventEmitter<string>();
+  
+  onSearch() {
+    this.searchOutput.emit(this.search)
+  }
+
+}
--- src/app/modules/characters/characters-page/characters-page.component.ts
+++ src/app/modules/characters/characters-page/characters-page.component.ts
@@ -2,18 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { CharactersService } from '../services/characters.service';
 import { CardComponent } from '../../../global/components/card/card.component';
 import { PaginatorComponent } from '../../../global/components/paginator/paginator.component';
+import { FilterComponent } from '../../../global/components/filter/filter.component';
 import { ApiResponse } from '../../../global/models/api-response.model';
 import { Character } from '../models/character.model';
 
+
 @Component({
   selector: 'app-characters-page',
-  imports: [CardComponent, PaginatorComponent],
+  imports: [CardComponent, PaginatorComponent, FilterComponent],
   templateUrl: './characters-page.component.html',
   styleUrl: './characters-page.component.css'
 })
 export class CharactersPageComponent implements OnInit{
 
   charactersResponse: ApiResponse<Character[]> = {
+    count: 0,
+    next: '',
+    previous: '',
+    results: []
+  };
+
+  filteredCharactersResponse: ApiResponse<Character[]> = {
     count: 0,
     next: '',
     previous: '',
@@ -29,8 +38,8 @@ export class CharactersPageComponent implements OnInit{
   loadCharacters(page: string = ''): void {
     this.charactersService.getCharactersList(page).subscribe({
       next: (response: ApiResponse<Character[]>) => {
-        console.log(response);
         this.charactersResponse = response;
+        this.filteredCharactersResponse = response;
       },
       error: (error) => {
         console.error(error);
@@ -41,8 +50,19 @@ export class CharactersPageComponent implements OnInit{
     });
   }
 
+  onSearch(search: string): void {
+    if(search === '') {
+      this.loadCharacters();
+      return;
+    }
+    this.filteredCharactersResponse.results = this.charactersResponse.results.filter((item: Record<string, any>) =>
+      Object.values(item).some((value: any) =>
+      String(value).toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  }
+
   onPageChange(page: number): void {
-    console.log(page);
     this.loadCharacters(page.toString());
   }
 }
