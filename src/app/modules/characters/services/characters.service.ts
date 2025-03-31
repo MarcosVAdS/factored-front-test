02fc61949303fffcebc65a9bad1b2e9aa4ef7e87
Marcos Almeida <victor.kernel14@gmail.com>
2025-03-31 12:21:12 -0300

    feature: implements share replay on characters servire to implements a cache approach

--- src/app/modules/characters/services/characters.service.ts
+++ src/app/modules/characters/services/characters.service.ts
@@ -3,21 +3,24 @@ import { Injectable } from '@angular/core';
 import { BaseApiService } from '../../../global/services/base-api.service';
 import { ApiResponse } from '../../../global/models/api-response.model';
 import { Character } from '../models/character.model';
-import { Observable } from 'rxjs';
+import { Observable, shareReplay } from 'rxjs';
 import { environment } from '../../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CharactersService extends BaseApiService<Character> {
+  private cache!: Observable<any>
+
   constructor(protected override http: HttpClient) {
     super(http, environment.starWarsApiUrl);
   }
 
   getCharactersList(page: string = ''): Observable<ApiResponse<Character[]>> {
-    return this.get(`/people/?page=${page}`, true) as Observable<
-      ApiResponse<Character[]>
-    >;
+    if (!this.cache) {
+      this.cache = this.get(`/people/?page=${page}`, true).pipe(shareReplay(1))
+    }
+    return this.cache
   }
 
   getCharacterDetails(id: number): Observable<Character> {
